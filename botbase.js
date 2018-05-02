9f258db5fb598cf8f47a7c774d8d629bbe4ce7a1
Richard Key <rich@busyrich.com>
2018-05-02 11:01:35 -0700

    Added datatype/attributes.

--- botbase.js
+++ botbase.js
@@ -1,6 +1,12 @@
 const connectors = require('./lib/connectors'),
-      db = connectors.MongoDB(),
-      members = db.model('members');
+      dataTypes = require('./lib/datatypes'),
+      db = connectors.MongoDB();
+
+const members = db.model('members', {
+  username: dataTypes.TEXT,
+  age: dataTypes.INT,
+  lastping: dataTypes.DATE
+});
 
 members.nuke({username:'dudethree'}, (error) => {
   console.log(error);
