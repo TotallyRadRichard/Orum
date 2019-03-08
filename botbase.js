3ea604e5e8ea4e13d72b757b5a9c40ca868f22ca
Richard Key <rich@busyrich.com>
2019-03-08 00:09:29 -0700

    Updated connector registration to be at runtime rather than loadtime.

--- botbase.js
+++ botbase.js
@@ -1,19 +1,30 @@
-const connectors = require('./lib/connectors'),
-      BotBase = {};
+const BotBase = {};
 
 BotBase.type = require('./lib/datatypes');
+
+// Collection of database connectors to use
+// pre-filled with built in connectors
+BotBase.connectors = {
+  'mongodb': './lib/connectors/mongodb/connector.mongo'
+};
 
 BotBase.connect = function(configuration, onConnect) {
   if(!configuration || !configuration.connector) {
     return onConnect(new Error('You must specify a connector type in [configuration.connector].'));
   }
 
-  switch(configuration.connector.toLowerCase()) {
-    case 'mongodb':
-      return connectors.MongoDB(configuration, onConnect);
-      break;
-    default:
-      onConnect(new Error(`No connector found for ${configuration.connector}`));
+  if(BotBase.connectors[configuration.connector]) {
+    BotBase.connectors[configuration.connector](configuration, onConnect);
+  } else {
+    onConnect(new Error(`No connector found for ${configuration.connector}`));
+  }
+};
+
+BotBase.connector = function(name, path) {
+  if(BotBase.connectors[name] && typeof BotBase.connectors[name] === 'string') {
+    BotBase.connectors[name] = require(BotBase.connectors[name]);
+  } else if(BotBase.connectors[name]) {
+    BotBase.connectors[name] = require(path);
   }
 };
 
--- test.js
+++ test.js
@@ -1,23 +1,24 @@
 const bb = require('./botbase');
+// bb.connector('mongodb');
 
-const db = bb.connect({connector:'mongodb'}, function(error) {
-  if(error) {
-    throw error;
-  }
-});
+// const db = bb.connect({connector:'mongodb'}, function(error) {
+//   if(error) {
+//     throw error;
+//   }
+// });
 
-const members = db.model('members', {
-  username:  bb.type.TEXT,
-  age: bb.type.INT,
-  lastping: bb.type.INT
-});
+// const members = db.model('members', {
+//   username:  bb.type.TEXT,
+//   age: bb.type.INT,
+//   lastping: bb.type.INT
+// });
 
-let username = "Member 171051";
-//username = 'Member ' + (Math.round(Math.random() * 999999) + 100000);
+// let username = "Member 171051";
+// //username = 'Member ' + (Math.round(Math.random() * 999999) + 100000);
 
-members.updateOrCreate({username:username}, {$set: {
-  age: 0,
-  lastping: Date.now()
-}}, (error, created) => {
-  console.log(error || created);
-});
+// members.updateOrCreate({username:username}, {$set: {
+//   age: 0,
+//   lastping: Date.now()
+// }}, (error, created) => {
+//   console.log(error || created);
+// });
